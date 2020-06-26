5e7b1912820e707f999f67fd43d4c94e708a623e
sgsettle <s.settle41@gmail.com>
2020-06-25 20:45:57 -0400

    weather

--- onedayproject/src/App.js
+++ onedayproject/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+// import { render } from "react-dom";
+import OpenWeather from './components/OpenWeather/OpenWeather';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  }
 }
+
+  render() {
+    return (
+      <div>
+        <OpenWeather />
+      </div>
+    );
+  }
+}
+
+// render(<App />, document.getElementById("root"));
 
 export default App;
