32278d0a280eb72500704e34f5810bb8257660d4
KateLockhart <katelockhart707@gmail.com>
2020-06-25 21:02:59 -0400

    nasa

--- onedayproject/src/App.js
+++ onedayproject/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { Component } from "react";
+import { render } from "react-dom";
 import './App.css';
+import earthPic from './Assets/earth.png';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import NASA from './Components/NASA/NASA';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <div class='navbar'>
+          <img id='earth' src={earthPic} />
+          <h2 id='title'>Here you are, but what to do?</h2>
+        </div>
+        <NASA />
+        <div class='footer'>
+          <p class='footNote'>
+            While using this site, you agree to have read and accepted our terms of use, cookie and privacy policy.
+            Copyright 2020 by 404 Team Not Found Co. All Rights Reserved.
+          </p> 
+          <p class='footNote'>Powered by Legacy React, Sass, and Web Dev Student Tears.</p>
+        </div>
+      </div>
+    );
+  }
 }
+
+//render(<App />, document.getElementById("root"));
 
 export default App;
---
+++ onedayproject/src/Components/NASA/NASA.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './NASA.css';
+
+export default class NASA extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            lat: null,
+            long: null,
+            NASAurl: 'https://api.nasa.gov/planetary/earth/assets',
+            key: 'evU2fnvvCNuGqEmm8nZ2aDzEFl6ATasCjVFujLvY',
+            img: ''
+        }
+    }
+
+    componentWillMount() {
+        console.log("NASA API is about to mount.");
+    }
+
+    componentDidMount = (props) => {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition((position) => {
+            this.setState({
+                long: (position.coords.longitude),
+                lat: (position.coords.latitude)
+            })
+
+            console.log("Latitude is :", position.coords.latitude);
+            console.log("Longitude is :", position.coords.longitude);
+            console.log("NASA component sucessfully mounted.");
+
+            fetch(`${this.state.NASAurl}?lon=${this.state.long}&lat=${this.state.lat}&dim=0.10&api_key=${this.state.key}`)
+            .then(res => res.json())
+            .then(json => {
+                console.log(json);
+                this.setState({
+                    img: json.url
+                })
+                console.log(this.state.img);
+            })
+          })
+        }
+      }
+    
+    render(){
+        return(
+            <div id='nasaDiv'>
+                <h2 id='nasah2'>Where in the world are you? Let's take a look from above:</h2>
+                <br />
+                <img id='apiReturn' src={this.state.img} alt='NASA API photo'/>
+                <br />
+                <hr />
+            </div>
+        )
+    }
+}
